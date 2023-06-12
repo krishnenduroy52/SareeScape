a797660e4fae323ceac76b56027ffff6a60a97f8
krishnendu roy <krishnendu.kalyani1@gmail.com>
2023-06-13 00:28:36 +0530

    some minor changes on festtivecollection

--- client/src/compnents/Festtivecollection.jsx
+++ client/src/compnents/Festtivecollection.jsx
@@ -16,7 +16,7 @@ const Festivecollection = () => {
           <div className="product-price">
             <div>
               <span
-                className="money"
+                className="money-sale"
                 doubly-currency-inr="684400"
                 doubly-currency="INR"
               >
@@ -36,29 +36,54 @@ const Festivecollection = () => {
       </div>
       <div className="festive-products">
         <a href="">
-          <img src="https://cdn.shopify.com/s/files/1/0560/2227/0115/products/2_70367a76-fb2d-4006-89e0-e89a1e87c425_360x.jpg?v=1643278461" />
+          <img src="https://cdn.shopify.com/s/files/1/0574/2359/3651/files/SP0080921CC02EY0327_1_14aa1c06-9750-40ee-8af2-d4d310f0d590.jpg?v=1685159542" />
           <span className="sale">Sale</span>
           <div className="product-details">
-            <p>
-              PEACH AND GOLD COLORED ART SILK WOVEN DESIGN BANARASI HANDLOOM
-              SAREE
-            </p>
+            <p>Printed Pure Cotton Saree In Black</p>
           </div>
           <div className="product-price">
             <div>
               <span
-                className="money"
+                className="money-sale"
                 doubly-currency-inr="684400"
                 doubly-currency="INR"
               >
-                ₹ 6,844.00 INR
+                ₹ 6,499.00 INR
               </span>
               <span
                 class="money"
                 doubly-currency-inr="345000"
                 doubly-currency="INR"
               >
-                ₹ 3,450.00 INR
+                ₹ 1,950.00 INR
+              </span>
+            </div>
+            <span class="grid-product__price--savings">Save 50%</span>
+          </div>
+        </a>
+      </div>
+      <div className="festive-products">
+        <a href="">
+          <img src="https://cdn.shopify.com/s/files/1/0574/2359/3651/files/SH0090522LL02Y00289_1.jpg?v=1684857345" />
+          <span className="sale">Sale</span>
+          <div className="product-details">
+            <p>Black Pure Linen Ethnic Motifs Design Jamdani</p>
+          </div>
+          <div className="product-price">
+            <div>
+              <span
+                className="money-sale"
+                doubly-currency-inr="684400"
+                doubly-currency="INR"
+              >
+                ₹ 18,999.00 INR
+              </span>
+              <span
+                class="money"
+                doubly-currency-inr="345000"
+                doubly-currency="INR"
+              >
+                ₹ 8,999.00
               </span>
             </div>
             <span class="grid-product__price--savings">Save 50%</span>
@@ -78,7 +103,7 @@ const Festivecollection = () => {
           <div className="product-price">
             <div>
               <span
-                className="money"
+                className="money-sale"
                 doubly-currency-inr="684400"
                 doubly-currency="INR"
               >
@@ -98,91 +123,26 @@ const Festivecollection = () => {
       </div>
       <div className="festive-products">
         <a href="">
-          <img src="https://cdn.shopify.com/s/files/1/0560/2227/0115/products/2_70367a76-fb2d-4006-89e0-e89a1e87c425_360x.jpg?v=1643278461" />
+          <img src="https://cdn.shopify.com/s/files/1/0664/7676/9521/files/21441-42a_480x480.jpg?v=1670845957" />
           <span className="sale">Sale</span>
           <div className="product-details">
-            <p>
-              PEACH AND GOLD COLORED ART SILK WOVEN DESIGN BANARASI HANDLOOM
-              SAREE
-            </p>
+            <p>Ranas Banarsi Silk Saree</p>
           </div>
           <div className="product-price">
             <div>
               <span
-                className="money"
+                className="money-sale"
                 doubly-currency-inr="684400"
                 doubly-currency="INR"
               >
-                ₹ 6,844.00 INR
+                ₹ 10,000.00 INR
               </span>
               <span
                 class="money"
                 doubly-currency-inr="345000"
                 doubly-currency="INR"
               >
-                ₹ 3,450.00 INR
-              </span>
-            </div>
-            <span class="grid-product__price--savings">Save 50%</span>
-          </div>
-        </a>
-      </div>
-      <div className="festive-products">
-        <a href="">
-          <img src="https://cdn.shopify.com/s/files/1/0560/2227/0115/products/2_70367a76-fb2d-4006-89e0-e89a1e87c425_360x.jpg?v=1643278461" />
-          <span className="sale">Sale</span>
-          <div className="product-details">
-            <p>
-              PEACH AND GOLD COLORED ART SILK WOVEN DESIGN BANARASI HANDLOOM
-              SAREE
-            </p>
-          </div>
-          <div className="product-price">
-            <div>
-              <span
-                className="money"
-                doubly-currency-inr="684400"
-                doubly-currency="INR"
-              >
-                ₹ 6,844.00 INR
-              </span>
-              <span
-                class="money"
-                doubly-currency-inr="345000"
-                doubly-currency="INR"
-              >
-                ₹ 3,450.00 INR
-              </span>
-            </div>
-            <span class="grid-product__price--savings">Save 50%</span>
-          </div>
-        </a>
-      </div>
-      <div className="festive-products">
-        <a href="">
-          <img src="https://cdn.shopify.com/s/files/1/0560/2227/0115/products/2_70367a76-fb2d-4006-89e0-e89a1e87c425_360x.jpg?v=1643278461" />
-          <span className="sale">Sale</span>
-          <div className="product-details">
-            <p>
-              PEACH AND GOLD COLORED ART SILK WOVEN DESIGN BANARASI HANDLOOM
-              SAREE
-            </p>
-          </div>
-          <div className="product-price">
-            <div>
-              <span
-                className="money"
-                doubly-currency-inr="684400"
-                doubly-currency="INR"
-              >
-                ₹ 6,844.00 INR
-              </span>
-              <span
-                class="money"
-                doubly-currency-inr="345000"
-                doubly-currency="INR"
-              >
-                ₹ 3,450.00 INR
+                ₹ 5,000.00 INR
               </span>
             </div>
             <span class="grid-product__price--savings">Save 50%</span>
